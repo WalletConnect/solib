85cc61d7f6b9bf9c800e5e6d8fcf5dc3df44f8e6
Celine Sarafa <celinesarafa@gmail.com>
2022-10-11 15:06:40 +0300

    Update connector utils to use static store

--- src/utils/connector.ts
+++ src/utils/connector.ts
@@ -4,7 +4,7 @@ import Store from "../store";
 export async function withConnector<T>(
   withConnectorFunc: (connector: Connector) => Promise<T>
 ) {
-  const connector = new Store().getActiveConnector();
+  const connector = Store.getActiveConnector();
 
   if (connector.isAvailable()) {
     return await withConnectorFunc(connector);
