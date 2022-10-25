2efbc5413c90de431b998bd1cf5a25c72c10895a
Celine Sarafa <celinesarafa@gmail.com>
2022-10-25 15:59:48 +0300

    Export transaction type

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export * from './connectors'
 export type { Cluster } from './types/cluster'
 export type { StoreConfig } from './store/index'
 export type { WalletConnectAppMetadata } from './connectors/walletconnect'
+
+export type { TransactionResult } from './types/transaction'
