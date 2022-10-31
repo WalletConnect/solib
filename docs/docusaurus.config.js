1c32a8ea9b0e309868827fcc979219a9085252fe
Chad Jackson <chadyj@users.noreply.github.com>
2022-10-31 13:02:26 +0100

    fix broken footer link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -64,8 +64,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro'
+                label: 'Getting Started',
+                to: '/'
               }
             ]
           },
